645d49a0dce5a3c6e713ffde7ca9701740ffe42c
QueenterJuma <jumahqueenter@gmail.com>
2023-05-17 16:16:36 +0300

    Modify student.rb file

--- student.rb
+++ student.rb
@@ -1,12 +1,18 @@
 require_relative 'person'
 
 class Student < Person
+  attr_reader : classroom
+
   def initialize(age, classroom, parent_permission: true, name: 'Unknown')
     super(age, parent_permission: parent_permission, name: name)
-    @classroom = classroom
   end
 
   def play_hooky
     '¯\(ツ)/¯'
+  end
+  def classroom=(classroom)
+    @classroom = classroom
+    @classroom.students.push(self) unless @classroom.students.include?
+    (self)
   end
 end
