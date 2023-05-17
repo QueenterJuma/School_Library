d500a82ff6f7dfb0e3f63d54b3bd828dbf66004f
QueenterJuma <jumahqueenter@gmail.com>
2023-05-17 17:03:16 +0300

    Fixed linters

--- student.rb
+++ student.rb
@@ -13,7 +13,6 @@ class Student < Person
 
   def classroom=(classroom)
     @classroom = classroom
-    @classroom.students.push(self) unless @classroom.students.include?
-    (self)
+    @classroom.students.push(self) unless @classroom.students.include? self
   end
 end
